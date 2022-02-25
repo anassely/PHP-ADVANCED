c16d56dcc7d8c84a1e1a7e379575002d1827d859
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-02-25 22:47:49 +0100

    new

---
+++ 1-Arrays/Opdracht-1.1/index.php
@@ -0,0 +1,10 @@
+<?php
+
+ $anass = array("Voornaam" => "anass", "Achternaam" => "yah", "Leeftijd" => 17, "Klas" => "4");
+
+
+ echo "Gegevens anass: Voornaam: " . $anass["Voornaam"] . " Achternaam: ".$anass["Achternaam"] . " Leeftijd: " . $anass["Leeftijd"] . " Klas: " . $anass["Klas"];
+
+
+
+ 
