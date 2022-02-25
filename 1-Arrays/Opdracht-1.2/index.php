5bcc618dba73d200b87515c9dedc96eca392748f
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-02-25 22:51:42 +0100

    new

---
+++ 1-Arrays/Opdracht-1.2/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$persoon  = array("Voornaam" => "jack", "Achternaam" => "hamza", "Leeftijd" => 21, "Opleiding" => "rechter");
+echo "Gegevens jack: Voornaam: " . $persoon ["Voornaam"] . " Achternaam: ".$persoon ["Achternaam"] . " Leeftijd: " . $persoon ["Leeftijd"] . " Opleiding: " . $persoon ["Opleiding"];
+
