966fcf97c29758483d4e8bae0bc8466546b49ba6
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-03-04 10:24:02 +0100

    new

--- 4-Functions/Opdracht-4.2/index.php
+++ 4-Functions/Opdracht-4.2/index.php
@@ -4,23 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
 </head>
 <body>
-    
 <?php
-function rekenUit($num1, $num2, $num3){
-   $antwoord = $num1 + $num2 + $num3;
-   return $antwoord;
+function rekenUit($nummer1, $nummer2, $nummer3){
+    $antwoord = $nummer1 + $nummer2 + $nummer3;
+    return $antwoord;
 }
 
-$answer1 = rekenUit(5, 10, 5);
-$answer2 = rekenUit(43365, 84396398, 962694);
-$answer3 = rekenUit(5511984.266484, 145867225.7, 5);
+echo "als ik de getallen 7,1 en 3 bij elkaar optel, is de uitkomst" . $answer1 = rekenUit(7, 1, 3). "<br>";
+echo "als ik de getallen 3,1 en 5 bij elkaar optel, is de uitkomst" . $answer2 = rekenUit(3, 1, 5). "<br>";
+echo "als ik de getallen 24,41 en 88 bij elkaar optel, is de uitkomst" . $answer3 = rekenUit(24, 41, 88). "<br>";
 
-echo $answer1 . "<br>";
-echo $answer2 . "<br>";
-echo $answer3 . "<br>";
 ?>
 </body>
-</html>
