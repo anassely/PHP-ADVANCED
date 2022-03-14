e9d36227818a283a18fc8c1405de88fcbfb4cf6f
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-03-14 09:14:30 +0100

    new

--- Eindopdracht/index1.php
+++ Eindopdracht/index1.php
@@ -4,28 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>abdel's PHP ADVANCED EINDOPDRACHT</title>
+    <title>Nassim PHP advanced eindopdracht</title>
 </head>
 <body>
 <center>
  <form name="invoer" action="advanced-2.php" method="post">
     <option>tekst-kleuren</option>
     <select name="tekst-kleuren" id="tekst-kleuren">
-    <?php $kleuren = array ("pink", "blue", "green", "black", "brown", "red");
+    <?php $kleuren = array ("pink", "cyan", "green", "black", "brown", "red");
     foreach($kleuren as $kleur){
         echo "<option value = $kleur> $kleur</option>"; }
-  
+
  ?>
  </select>
  <option>achtergrond-kleuren</option>
  <select name="achtergrond-kleuren" id="achtergrond-kleuren">
-     <?php $kleuren = array(" brown", "blue", "green", "black", "red", "pink"); 
+     <?php $kleuren = array(" yellow", "blue", "purple", "black", "orange", "pink"); 
      foreach($kleuren as $achtergrond_color){
          echo "<option value = $achtergrond_color>$achtergrond_color</option>";
      }
      ?>
 
- </select>  
+ </select>
  <div>
    <option>tabel-bord (px)</option>
   <input type="text" name="tbname">
@@ -39,6 +39,6 @@
          </div>
  <form>
 
-</center>
+    </center>
 </body>
 </html>
--- Eindopdracht/index2.php
+++ Eindopdracht/index2.php
@@ -4,11 +4,11 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Nassim PHP advanced eindopdracht</title>
 </head>
 <body>
   <style>
-    
+
 body {
   color: <?= $_POST["tekst-kleuren"] ?>;
   background-color: <?= $_POST["achtergrond-kleuren"] ?>;
@@ -18,7 +18,7 @@ table,tr,td{
     padding:<?= $_POST['padding']?>px;
 }
 </style>
-<center>'
+<center>
 
  <?php function maakRij($mijngegevens1,$value){
      echo "<tr>
@@ -30,7 +30,7 @@ table,tr,td{
   <table>
    <?php
    $mijngegevens=array(
-   "naam"=> " anass","Leefdtijd"=> 18,"artiest"=> "Playboi Carti","woonplaats"=>"Amsterdam","sport"=> "Voetbal");
+   "naam"=> "Nassim Omaati","Leefdtijd"=> 17,"artiest"=> "Kanye West","woonplaats"=>"Amsterdam","games"=> "dying light");
    foreach($mijngegevens as $mijngegevens1 => $value){
       maakRij($mijngegevens1,$value);
    }
