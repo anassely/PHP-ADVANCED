ab12aa82fec236b92b00d782e1c1719e3fe19faa
anass.el.yahiaoui <anass.yahiaoui@student.rocva.nl>
2022-03-14 09:15:26 +0100

    new

--- Eindopdracht/index1.php
+++ Eindopdracht/index1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nassim PHP advanced eindopdracht</title>
+    <title>anass PHP advanced eindopdracht</title>
 </head>
 <body>
 <center>
